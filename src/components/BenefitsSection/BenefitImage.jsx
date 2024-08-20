e987a64bd6452d1309790f4affc22ff32f0d9dcd
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-08-20 13:28:04 +0300

    change and import new cover section for benefit section

--- src/components/BenefitsSection/BenefitImage.jsx
+++ src/components/BenefitsSection/BenefitImage.jsx
@@ -1,10 +1,14 @@
-import benefitImage from "../../assets/images/manhattan-skyline-background-scaled.webp";
+import benefitImage from "../../assets/images/wallpaperflare.com_wallpaper.webp";
 
 function BenefitImage() {
   return (
     <>
       <div className="col-span-12 md:col-span-6 relative  flex justify-end items-center">
-        <img src={benefitImage} className="size-full" alt="American Flag" />
+        <img
+          src={benefitImage}
+          className="size-full object-cover"
+          alt="American Flag"
+        />
       </div>
     </>
   );
