fe2a1be54c8ace51fe7fdd1769655593c2ffd094
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-08-20 14:19:51 +0300

    update steps section

--- src/components/PackageSection/PackageSteps.jsx
+++ src/components/PackageSection/PackageSteps.jsx
@@ -8,27 +8,37 @@ function PackageSteps() {
     {
       id: 1,
       step: "Company Formation",
-      icon: <BsBuildings className="text-primary-color2 text-5xl" />,
+      icon: (
+        <BsBuildings className="text-primary-color text-5xl group-hover:text-primary-color2" />
+      ),
     },
     {
       id: 2,
       step: "EIN",
-      icon: <FaFileInvoiceDollar className="text-primary-color2 text-5xl" />,
+      icon: (
+        <FaFileInvoiceDollar className="text-primary-color text-5xl group-hover:text-primary-color2" />
+      ),
     },
     {
       id: 3,
       step: "Registered Agent",
-      icon: <FaRegHandshake className="text-primary-color2 text-5xl" />,
+      icon: (
+        <FaRegHandshake className="text-primary-color text-5xl group-hover:text-primary-color2" />
+      ),
     },
     {
       id: 4,
       step: "US Bank Account",
-      icon: <CiCreditCard1 className="text-primary-color2 text-5xl" />,
+      icon: (
+        <CiCreditCard1 className="text-primary-color text-5xl group-hover:text-primary-color2" />
+      ),
     },
     {
       id: 5,
       step: "Post Formation Support",
-      icon: <BiMessageRoundedDetail className="text-primary-color2 text-5xl" />,
+      icon: (
+        <BiMessageRoundedDetail className="text-primary-color text-5xl group-hover:text-primary-color2" />
+      ),
     },
   ];
   return (
@@ -36,18 +46,26 @@ function PackageSteps() {
       <h2 className="text-3xl md:text-5xl font-semibold text-center mb-12 underLine">
         Your all in one Package
       </h2>
-      <div className="wrapper grid  grid-cols-12 gap-y-16">
+      <div className="wrapper grid  grid-cols-12 gap-y-16 px-4">
         {formationSteps.map((step, index) => {
           return (
             <article
               key={step.id}
-              className="col-span-12 md:col-span-6 lg:col-span-4"
+              className="col-span-12 md:col-span-6 lg:col-span-4 px-2 group duration-300"
             >
-              <div className="flex justify-center items-center flex-col gap-3">
-                <span> {step.icon}</span>
-                <h3 className="text-2xl font-medium">{`${index + 1}- ${
-                  step.step
-                }`}</h3>
+              <div className="flex h-full group-hover:bg-primary-bg-yellow duration-300  bg-primary-bg-darkBlue2 py-8 px-6 rounded-3xl justify-center items-start flex-col gap-3">
+                <div className="bg-primary-bg-gray2 size-1 group-hover:bg-primary-bg-darkBlue2 rounded-circle flex justify-center items-center p-10">
+                  <span> {step.icon}</span>
+                </div>
+                <h3 className="text-2xl font-bold text-primary-color2 group-hover:text-primary-color duration-300">{`${
+                  index + 1
+                }- ${step.step}`}</h3>
+                <p className="text-primary-color4 leading-7 group-hover:text-primary-color duration-300">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Aliquam voluptatum eius ipsa ad harum enim iusto, in, sequi
+                  fugit velit blanditiis doloremque maiores, quidem earum minima
+                  praesentium molestias est illo!
+                </p>
               </div>
             </article>
           );
