e97bc9c2682ae652d1bca86efd804fc9e4339aa9
MohamedNagy0 <mohamednagy.mis@gmail.com>
2024-08-20 10:49:42 +0300

    Navbar Style Change

--- src/components/myNav/MyNav.jsx
+++ src/components/myNav/MyNav.jsx
@@ -1,22 +1,15 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { RiMenu3Line } from "react-icons/ri";
 import logoImag from "../../assets/images/Logo-B-01-e1723398545101.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function MyNav() {
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
 
-  const navLinks = [
-    { id: 1, name: "Home" },
-    { id: 2, name: "About" },
-    { id: 3, name: "Services" },
-    { id: 4, name: "Reviews" },
-    { id: 5, name: "Contacts" },
-  ];
   return (
     <>
       <Navbar
@@ -33,33 +26,52 @@ function MyNav() {
               />
             </Link>
           </h1>
+
           <Navbar.Toggle
             onClick={handleShow}
             aria-controls="basic-navbar-nav"
-            className="focus:shadow-none border-none p-0 active:scale-75 flex items-center justify-center"
+            className="focus:shadow-none border-none flex items-center justify-center"
           >
             <RiMenu3Line className="text-3xl" />
           </Navbar.Toggle>
+
           <Navbar.Collapse
             id="basic-navbar-nav"
-            className="bg-primary-bg-darkBlue2 lg:bg-transparent mt-2 text-primary-color2 rounded-xl"
+            className=" lg:bg-transparent mt-2 "
           >
-            <Nav className="me-auto py-3 ps-4 lg:hidden">
-              {navLinks.map((link) => {
-                return (
-                  <Fragment key={link.id}>
-                    <Nav.Link className="hover:text-primary-bg-yellow text-primary-color2  group">
-                      <span className="font-bold group-hover:bg-white group-hover:text-primary-color group-hover:duration-300 px-2 py-2 rounded-3xl">
-                        {link.name}
-                      </span>
-                    </Nav.Link>
-                  </Fragment>
-                );
-              })}
+            <Nav className="me-auto py-3 ps-4 lg:hidden gap-y-4">
+              <h3>
+                <NavLink
+                  className={(isActive) =>
+                    ` text-primary-color font-medium text-lg navActive  ${
+                      isActive ? "after:w-full" : ""
+                    }`
+                  }
+                >
+                  Home
+                </NavLink>
+              </h3>
+              <h3>
+                <Link className="text-primary-color font-medium text-lg navActive">
+                  Services
+                </Link>
+              </h3>
+              <h3>
+                <Link className="text-primary-color font-medium text-lg navActive">
+                  Reviews
+                </Link>
+              </h3>
+              <h3>
+                <Link className="text-primary-color font-medium text-lg navActive">
+                  Contacts
+                </Link>
+              </h3>
             </Nav>
+
             <a
-              href="#"
-              className="hidden lg:block lg:ms-auto primaryBtn text-primary-color"
+              href="#PackageSection"
+              className="hidden lg:block lg:ms-auto primaryBtn 
+              hover:bg-white border-2 border-primary-color2 hover:border-primary-color duration-300"
             >
               Apply Now
             </a>
